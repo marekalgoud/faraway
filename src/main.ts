b7ca7b23bdf7f99fd0819d3431ea9b6a0b9fe45f
Marek Algoud <marek.algoud@gmail.com>
2025-11-22 09:56:34 +0100

    fix: correction build netlify

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,3 @@
-// Polyfills must be imported first
-import './polyfills';
-
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
